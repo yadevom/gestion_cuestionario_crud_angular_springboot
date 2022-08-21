dbefc56b63e68c9d00ee2a3ebda535eb113ada8c
Yamid OM <yaormu@gmail.com>
2022-08-21 11:26:02 -0500

    ajustes aplicar jwt

--- sistema-examenes-back/src/main/java/com/sistema/examenes/services/impl/UserDetailsServiceImpl.java
+++ sistema-examenes-back/src/main/java/com/sistema/examenes/services/impl/UserDetailsServiceImpl.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 // Cargar y buscar usuario por el username
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
@@ -21,4 +23,5 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         }
         return user;
     }
+
 }
